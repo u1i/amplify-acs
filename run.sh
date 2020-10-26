163dec011a9af453a2e108dc4f331fc3ef96b2c1
Uli Hitzel <uli.hitzel@gmail.com>
2020-10-26 10:49:42 +0000

    changed

---
+++ run.sh
@@ -0,0 +1,19 @@
+project=uli-hello-$RANDOM
+image=u1ih/hello
+
+#acs publish $project --delete_oldest --force --image $image --app_version 0.1
+
+#acs config --set PORT=8080 $project
+
+# amplify acs login
+acs new $project --force
+acs config --set PORT=8080 $project
+acs server --set Small $project
+
+acs publish $project --delete_oldest --force --image $image --app_version 0.1
+
+acs list $project
+
+# update image
+# amplify acs publish simplenodeapp --delete_oldest --force --image simplenodeapp --app_version 0.2
+
